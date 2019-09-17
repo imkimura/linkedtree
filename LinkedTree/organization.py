34cebd02c6698466587b613e1d751dd01e624233
jkssilva <juliakimura@univem.edu.br>
2019-09-17 14:59:34 -0300

    enviando p nao perder

---
+++ LinkedTree/organization.py
@@ -0,0 +1,10 @@
+listwords = []
+dicteres = dict()
+lword = list(str(input("Digite uma série de caracteres: ")))
+
+for w in lword:
+    if len(listwords) > 0:
+        print("oi")
+    else:
+        listwords.append(dicteres["word"] = w)
+print(listwords)
