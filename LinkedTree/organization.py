05861159f591846f0c9ecf9de22b84f125d9f989
jkssilva <juliakimura@univem.edu.br>
2019-09-19 10:19:29 -0300

    Organização concluida.

--- LinkedTree/organization.py
+++ LinkedTree/organization.py
@@ -1,16 +1,24 @@
-listwords = []
-dicteres = dict()
-lword = list(str(input("Digite uma série de caracteres: ")))
+def organizeList(lword):
+    listwords = []
+    dicteres = dict()
+    for l in lword:
+        if len(listwords) > 0:
+            notexist = True
+            for w in range(0, len(listwords)):                        
+                if l == listwords[w]["word"]:
+                    notexist = False
+                    listwords[w]["freq"] += 1            
+            if notexist:
+                listwords.append(({"word":l,"freq":1}))                
+        else:        
+            listwords.append(({"word":l,"freq":1}))
+    return listwords
 
-for l in lword:
-    if len(listwords) > 0:
-        notexist = True
-        for w in range(0, len(listwords)):                        
-            if l == listwords[w]["word"]:
-                notexist = False
-                listwords[w]["freq"] += 1            
-        if notexist:
-            listwords.append(({"word":l,"freq":1}))                
-    else:        
-        listwords.append(({"word":l,"freq":1}))
-print(listwords)
+class Organization:        
+    lword = list(str(input("Digite uma série de caracteres: ")))
+    listwords = organizeList(lword)
+    print(listwords)
+
+if __name__ == "__main__":
+    Organization()
+    
